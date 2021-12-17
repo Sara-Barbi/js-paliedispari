89952817d132d7ec24744f60a85abb7a92625dfd
Sara-Barbi <barbisara02@gmail.com>
2021-12-17 16:09:04 +0100

    faccio vedere le ultime lettere

--- parola_palindroma/myscript.js
+++ parola_palindroma/myscript.js
@@ -1,8 +1,17 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma.
 
-
+let myArr = [];
 let parola = prompt("inserisci una parola palindroma!");
+let resto = parola.substring(3);
+
+myArr.push(resto);
+
+
+console.log(myArr);
+
+
+
 
 function miaVerifica(){
     
