22ecd10930b57f2c715d6c2a6c5ca89edb1fb497
Sara-Barbi <barbisara02@gmail.com>
2021-12-17 15:37:14 +0100

    prompt

--- parola_palindroma/myscript.js
+++ parola_palindroma/myscript.js
@@ -1,2 +1,9 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma.
+
+
+let parola = prompt("inserisci una parola palindroma!");
+
+function miaVerifica(){
+    
+}
