bd2c44d0f0a919ffeacbce4fb7ca7ef901da7b96
Sara-Barbi <barbisara02@gmail.com>
2021-12-18 14:31:39 +0100

    try for

--- parola_palindroma/myscript.js
+++ parola_palindroma/myscript.js
@@ -1,18 +1,19 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma.
 
-let myArr = [];
 let parola = prompt("inserisci una parola palindroma!");
-let resto = parola.substring(3);
 
-myArr.push(resto);
-
-
-console.log(myArr);
+let parolaInversa ='';
 
 
+function miaFunzione(){
+   for(i=parola.length; i>=0, i--;){
+       let carattere = parola[i];
+       parolaInversa += carattere;
+      
+   }
 
 
-function miaVerifica(){
-    
-}
+  
+
+};
