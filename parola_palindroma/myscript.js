0cdbd86eba15c84fae067b45e9817fbee1cc564e
Sara-Barbi <barbisara02@gmail.com>
2021-12-19 17:25:56 +0100

    function non funz

--- parola_palindroma/myscript.js
+++ parola_palindroma/myscript.js
@@ -10,32 +10,27 @@ let alContrario=[];
 
 
 
+function controllo(){
 
-
-    for(i=0; i<parolaDivisa.length; i++){
-
+    for(i=0; i<parolaDivisa.length; i++){    
       //  console.log(parolaDivisa[i]);
-    }
-
+    }    
     for(index=parolaDivisa.length; index>0; index--){
         
-      //  console.log(parolaDivisa[index -1]);
-
+      //  console.log(parolaDivisa[index -1]);    
         alContrario.push(parolaDivisa[index -1]);
-    }
+    }  
 
-    console.log(parolaDivisa, alContrario);
-
+    console.log(parolaDivisa, alContrario);    
+    
     if(parolaDivisa==alContrario){
-        document.writeln("parola palindroma")
+       return "parola palindroma"
     }else{
-        document.writeln("parola NON palindroma")
+       return "parola NON palindroma"    
+    }    
+}
 
-    }
-
-
-
-
+document.writeln(controllo());
 
 
 
