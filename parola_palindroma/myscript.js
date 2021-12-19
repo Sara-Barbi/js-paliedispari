dbffa501094bd2948c98418048c57a343e859bb6
Sara-Barbi <barbisara02@gmail.com>
2021-12-19 17:18:51 +0100

    palindroma non funzionante

--- parola_palindroma/myscript.js
+++ parola_palindroma/myscript.js
@@ -2,10 +2,36 @@
 //Creare una funzione per capire se la parola inserita è palindroma.
 
 let parola = prompt("inserisci una parola palindroma!");
+let parolaDivisa = parola.split('');
+
+let alContrario=[];
 
 
 
 
+
+
+
+    for(i=0; i<parolaDivisa.length; i++){
+
+      //  console.log(parolaDivisa[i]);
+    }
+
+    for(index=parolaDivisa.length; index>0; index--){
+        
+      //  console.log(parolaDivisa[index -1]);
+
+        alContrario.push(parolaDivisa[index -1]);
+    }
+
+    console.log(parolaDivisa, alContrario);
+
+    if(parolaDivisa==alContrario){
+        document.writeln("parola palindroma")
+    }else{
+        document.writeln("parola NON palindroma")
+
+    }
 
 
 
@@ -21,4 +47,4 @@ let parola = prompt("inserisci una parola palindroma!");
 
   
 
-};
+
