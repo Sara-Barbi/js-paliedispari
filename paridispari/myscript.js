42e804a8dbe4dc3418e0570bc469a4304dbd8c43
Sara-Barbi <barbisara02@gmail.com>
2021-12-19 11:52:27 +0100

    math.floor fuori da function, funzione per verific

---
+++ paridispari/myscript.js
@@ -0,0 +1,30 @@
+/*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.*/
+let scelta = prompt("scegli pari o dispari");
+let sceltaNum = parseInt(prompt("scegli un numero da 1 a 5"));
+let computerNum = Math.floor(Math.random() * 5)+1;
+let tot = sceltaNum + computerNum;
+
+
+
+function myFunction(){
+   if(tot%2==0 && scelta=="pari"){
+      return ("pari!, HAI VINTO")
+   }else if(tot%2!=0 && scelta=="pari"){
+       return ("dispari! HAI PERSO")
+   }else if(tot%2!=0 && scelta=="dispari"){
+       return("dispari! HAI VINTO")
+   }else if(tot%2==0 && scelta=="dispari"){
+       return("pari! HAI PERSO")
+   }
+};
+
+console.log(scelta,sceltaNum,computerNum,tot,myFunction());
+
+
+
+
+
